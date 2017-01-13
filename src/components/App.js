dcdd60f881ae295711898277454049b6ed866578
Brian Jacobel <bjacobel@gmail.com>
2017-01-13 13:40:44 -0500

    Add modal

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 
 import '../styles/App.scss';
 import Form from './Form';
+import ModalWrapper from './ModalWrapper';
 
 export default class App extends Component {
   render () {
     return (
-      <div>
+      <div className="app">
         <Form />
+        <ModalWrapper />
       </div>
     );
   }
